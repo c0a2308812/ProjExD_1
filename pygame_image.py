b49c593859a14aa4ff4984b961ea97dde5459dc1
setoyama <c0a2308812@edu.teu.ac.jp>
2024-06-11 16:03:44 +0900

    問4修正

--- pygame_image.py
+++ pygame_image.py
@@ -18,7 +18,9 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
+        kk_rct = kk_img.get_rect()
+        kk_rct.center = 300, 200
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(10)
