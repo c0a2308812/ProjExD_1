a965edfb1e9ee7ab83333546238ca4ab198fb7e5
setoyama <c0a2308812@edu.teu.ac.jp>
2024-06-11 16:54:53 +0900

    演習課題1

--- pygame_image.py
+++ pygame_image.py
@@ -26,7 +26,9 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
+
         key_lst = pg.key.get_pressed()
+        kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
@@ -34,7 +36,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            kk_rct.move_ip((2, 0))
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
