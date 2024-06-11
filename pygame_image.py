2a9c10f4451d2cbd1de50eeb0a979507ef30b77d
setoyama <c0a2308812@edu.teu.ac.jp>
2024-06-11 16:20:02 +0900

    問6

--- pygame_image.py
+++ pygame_image.py
@@ -4,7 +4,6 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
@@ -13,16 +12,18 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
         kk_rct = kk_img.get_rect()
         kk_rct.center = 300, 200
         screen.blit(kk_img, kk_rct)
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(200)
 
 
